37ec8062ec428704a1eff14f2e9f2fce0feee1e3
macabc <anej.batagelj@gmail.com>
2026-01-12 18:28:34 +0100

    fixing to right url for 20.215.135.129 slskd

--- src/main/java/org/muzika/playlistmanager/controllers/PlaylistController.java
+++ src/main/java/org/muzika/playlistmanager/controllers/PlaylistController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/playlist")
 public class PlaylistController {
 
     private final PlaylistService playlistService;
